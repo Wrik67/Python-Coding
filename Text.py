7d619c19d07e04c72c3c3e4adbd23edf744a4631
Wrik67 <pallab.037@gmail.com>
2025-12-30 15:46:27 +0600

    Add files via upload

---
+++ Text.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:"))
+
+if a > a and c:
+    print("The first number is the greatest number")
+elif b > b and c:
+    print("The second number is the greatest number")
+else:
+    print("The third number is the greatest number")
+    
