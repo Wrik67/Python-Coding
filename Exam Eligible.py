9cdbe9caaf7e0fe9a3a4ac46ed3b13444dd8b6ba
Wrik67 <pallab.037@gmail.com>
2025-12-30 16:01:14 +0600

    Add files via upload

---
+++ Exam Eligible.py
@@ -0,0 +1,19 @@
+cause = str(input("Do you have any medical cause: yes/no")).lower()
+
+if cause == "yes":
+  percentage = int(input("Enter you attendense percentage: "))
+  if percentage >=60:
+    print("You are eligible for the exam")
+  else:
+    print("You are not eligible for the exam")
+
+elif cause == "no":
+    percentage = int(input("Enter you attendense percentage: "))
+    if percentage >=75:
+          print("You are eligible for the exam")
+    else:
+
+          print("You are not eligible for the exam")
+
+else:
+     print("Invalid input!")
---
+++ Login system .py
@@ -0,0 +1,11 @@
+print("Login System")
+username = input("Enter your username:")
+
+if username == "Arham12345":
+  password = input("Enter your password:")
+  if password == "Sigmasigmasigma":
+    print("Login succsessfull !!")
+  else:
+    print("Incorrect password!Try again")
+else:
+  print("User not found")
