40a90309b79a459578dc94ae95de4d51f93a86b7
Wrik67 <pallab.037@gmail.com>
2026-01-08 17:30:44 +0600

    Add files via upload

--- 0 test .py
+++ 0 test .py
@@ -1,23 +1,16 @@
-row = int(input("Enter the number of rows:"))
+w = float(input("Enter your weight in KG: "))
+h = float(input("Enter your height in meter: "))
 
-if row % 2 == 0:
-    half = row // 2
+bmi = w / h ** 2
+print("Your bmi is: ", bmi)
+
+if bmi < 18.5:
+    print("You are under weight. Please gain some wait")
+elif 18.5 <= bmi <= 25.9:
+    print("Your weight is normal! Keep it up.")
+elif 25 <= bmi <= 29.9:
+    print("Your are over weight. Please loose some weight.")
+elif 30 <= bmi <= 34.9:
+    print("Your are obese. Loosing weight is emergency.")
 else:
-    half = row // 2 + 1
-space = half - 1
-
-for i in range(1, half + 1):
-    x = " " * space
-    y = "*" * (2 * i - 1)
-
-    print(x+y)
-    space = space - 1
-
-space = 1
-
-for i in range(half - 1,0,-1):
-    x = " " * space
-    y = "*" * (2 * i - 1)
-
-    print(x+y)
-    space = space + 1
+    print("Your are extremly obese. Please visit the doctor as soon as possible.")
