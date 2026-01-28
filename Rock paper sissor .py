e443a4c0ec0b04c9e5a1738ac0c87b3a95daa710
Wrik67 <pallab.037@gmail.com>
2026-01-28 16:16:55 +0600

    Add files via upload

--- Rock paper sissor .py
+++ Rock paper sissor .py
@@ -23,3 +23,48 @@ def rock_paper_scissors():
         user_score = 0
         computer_score = 0
         round_number = 1
+         
+        print("\n Welcome to Rock-Papper-Scissors Game!")
+        print("Type 'rules' to see how to play, or 'quit' to exit the game")
+
+        while True:
+            print(f"\n--- Round {round_number} ---")
+            print("Available choices: rock, papper, scissors.")
+            user_choice = input("Enter your choice: ").lower()
+
+            if user_choice == "quit":
+                print("\nThanks for playing")
+                print(f"Final score => You: {user_score} | Computer: {computer_score}\n")
+                break
+            elif user_choice == "rules":
+                display_rules()
+                continue
+            elif user_choice not in total_choices:
+                print("Invalid input. Please enter rock, papper, scissors.")
+                continue
+
+            computer_choice = random.choice(total_choices)
+            print(f"Computer chose: {computer_choice}")
+
+            result = determine_winner(user_choice, computer_choice)
+
+            if result == "tie":
+                print("It's a tie!")
+            elif result == "user":
+                print("You win this round!")
+                user_score += 1
+            else:
+                print("Computer wins this round!")
+                computer_score += 1
+
+            print(f"Current Scores => You: {user_score}  | Computer: {computer_score}")
+            round_number += 1
+
+        play_again = input("Would you like to play again? (yes/no):").lower()
+        if play_again != "yes":
+            print("Goodbye!")
+            break
+
+
+
+
