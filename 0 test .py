7f4cb40b8ce9c90b369787ccaac863a163e7c468
Wrik67 <pallab.037@gmail.com>
2026-03-04 15:05:03 +0600

    Add files via upload

--- 0 test .py
+++ 0 test .py
@@ -1,67 +1,66 @@
-TAX_RATE = 0.065
+import random
 
-menu = {
+def display_rules():
+    print("\n---Game rules---")
+    print("Rock beats Scissors")
+    print("Scissors beats Papper")
+    print("Papper beats Rock\n")
 
-1: ("Burger", 150.00),
-2: ("Fried Chicken", 175.00),
-3: ("Soda", 60.00),
-4: ("Salad", 120.00),
-5: ("Pizza", 500.00),
-6: ("Ice Cream", 150.00),
-7: ("Coffee", 200.00),
-8: ("Tea", 55.00),
-9: ("Juice", 90.00),
-10: ("Fried Rice", 210.00),
-11: ("Pasta", 250.00),
-12: ("Sandwich", 200.00),
-13: ("Water", 20.00),
-14: ("Meetbox", 250.00)
-}
+def determine_winner(user, computer):
+    if user == computer:
+        return "tie"
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "sissors" and computer == "papper") or \
+         (user == "papper" and computer == "rock"):
+        return "user"
+    else:
+        return "computer"
+    
+def rock_paper_scissors():
+    total_choices = ["rock", "paper", "scissors"]
 
-order = {}
-
-def show_menu():
-    print("\nMenu:")
-    for serial, (item, price) in menu.items():
-        print(f"{serial}. {item} - Tk. {price}")
-
-
-def take_order():
     while True:
-        choice = int(input("\nEnter item name or serial number (or 'done' to finish): ")).strip()
-        if choice.lower() == 'done':
+        user_score = 0
+        computer_score = 0
+        round_number = 1
+         
+        print("\n Welcome to Rock-Papper-Scissors Game!")
+        print("Type 'rules' to see how to play, or 'quit' to exit the game")
+
+        while True:
+            print(f"\n--- Round {round_number} ---")
+            print("Available choices: rock, papper, scissors.")
+            user_choice = input("Enter your choice: ").lower()
+
+            if user_choice == "quit":
+                print("\nThanks for playing")
+                print(f"Final score => You: {user_score} | Computer: {computer_score}\n")
+                break
+            elif user_choice == "rules":
+                display_rules()
+                continue
+            elif user_choice not in total_choices:
+                print("Invalid input. Please enter rock, papper, scissors.")
+                continue
+
+            computer_choice = random.choice(total_choices)
+            print(f"Computer chose: {computer_choice}")
+
+            result = determine_winner(user_choice, computer_choice)
+
+            if result == "tie":
+                print("It's a tie!")
+            elif result == "user":
+                print("You win this round!")
+                user_score += 1
+            else:
+                print("Computer wins this round!")
+                computer_score += 1
+
+            print(f"Current Scores => You: {user_score}  | Computer: {computer_score}")
+            round_number += 1
+
+        play_again = input("Would you like to play again? (yes/no):").lower()
+        if play_again != "yes":
+            print("Goodbye!")
             break
-
-        selected_item = None
-
-        if choice.isdigit() and int (choice) in menu:
-            selected_item = menu[int(choice)][0]
-        else:
-            print("Invalid selection. Please try again")
-
-def calculate_total():
-    total = 0.0
-    for item, quantity in order.items():
-        for _ , (menu_item, price) in menu.items():
-            if menu_item == item:
-                total += price * quantity
-                break
-            tax = total * TAX_RATE
-            return total, tax, total + tax
-        
-def print_bill():
-    print("\n---Bill Summary---")
-    total, tax, total_with_tax = calculate_total()
-    for item, quantity in order.items():
-        for _ , (menu_item, price) in menu.items():
-            if menu_item == item:
-                print(f"{item} X {quantity} = Tk. {price * quantity:2f}")
-                break
-    print(f"Subtotal: Tk. {total:.2f}")
-    print(f"Tax (6.7%): Tk. {tax:.2f}")
-    print(f"Total amount: Tk. {total_with_tax:.2f}")
-
-if __name__ == "__main__":
-    take_order()
-    print_bill()
-    print("\nThank you for dining with us!")
