d4b805001f0192091b87f77a8c4703ae148d5d5f
Wrik67 <pallab.037@gmail.com>
2026-03-08 17:09:51 +0600

    Add files via upload

--- 0 test .py
+++ 0 test .py
@@ -1,66 +1,29 @@
-import random
-
-def display_rules():
-    print("\n---Game rules---")
-    print("Rock beats Scissors")
-    print("Scissors beats Papper")
-    print("Papper beats Rock\n")
-
-def determine_winner(user, computer):
-    if user == computer:
-        return "tie"
-    elif (user == "rock" and computer == "scissors") or \
-         (user == "sissors" and computer == "papper") or \
-         (user == "papper" and computer == "rock"):
-        return "user"
+def calculator(choice, a, b):
+    if choice == 1:
+        return a + b
+    elif choice == 2:
+        return a - b
+    elif choice == 3:
+        return a * b
+    elif choice == 4:
+        if b == 0:
+            return "Division by zero is allowed."
+        return a / b
     else:
-        return "computer"
+        return "Invalid choice"
     
-def rock_paper_scissors():
-    total_choices = ["rock", "paper", "scissors"]
+print("-------Calculator-------")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplition")
+print("4. Divition")
+print("------------------------")
 
-    while True:
-        user_score = 0
-        computer_score = 0
-        round_number = 1
-         
-        print("\n Welcome to Rock-Papper-Scissors Game!")
-        print("Type 'rules' to see how to play, or 'quit' to exit the game")
+choice = int(input("Enter choice 1/2/3/4: "))
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
-        while True:
-            print(f"\n--- Round {round_number} ---")
-            print("Available choices: rock, papper, scissors.")
-            user_choice = input("Enter your choice: ").lower()
-
-            if user_choice == "quit":
-                print("\nThanks for playing")
-                print(f"Final score => You: {user_score} | Computer: {computer_score}\n")
-                break
-            elif user_choice == "rules":
-                display_rules()
-                continue
-            elif user_choice not in total_choices:
-                print("Invalid input. Please enter rock, papper, scissors.")
-                continue
-
-            computer_choice = random.choice(total_choices)
-            print(f"Computer chose: {computer_choice}")
-
-            result = determine_winner(user_choice, computer_choice)
-
-            if result == "tie":
-                print("It's a tie!")
-            elif result == "user":
-                print("You win this round!")
-                user_score += 1
-            else:
-                print("Computer wins this round!")
-                computer_score += 1
-
-            print(f"Current Scores => You: {user_score}  | Computer: {computer_score}")
-            round_number += 1
-
-        play_again = input("Would you like to play again? (yes/no):").lower()
-        if play_again != "yes":
-            print("Goodbye!")
-            break
+result = calculator(choice, a, b)
+print("--------------------------")
+print(f"Result: {result}")
+print("--------------------------")
