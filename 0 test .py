b9b15d188faeadccdd2caaff2a112b2a3589c66f
Wrik67 <pallab.037@gmail.com>
2026-03-17 15:45:13 +0600

    Add files via upload

--- 0 test .py
+++ 0 test .py
@@ -1,10 +1,18 @@
-months = str(input("Do you want to see all the months name (yes/no): ")).lower()
+marks = (10, 20, 30, 40, 50)
+fruits = ("apple", "banana", "cherry", "banana")
 
-if months == "yes":
-    print("The months name are: January, February, March, April, May, June, July, August, September, October, November and December.")
+print("1. Accessing an Element:", marks[2])
+print("2. Slicing:", marks[1:4])
+print("3. Length:", len(fruits))
+print("4. Count:", fruits.count("banana"))
+print("5. Index:", fruits.index("cherry"))
 
-if months == "no":
-    print("Goodbye")
+t3 = marks + fruits
+print("6. Concatenation:", t3)
 
-else:
-    print("Invalid input. Try again.")
+t4 = marks * 2
+print("7. Repitition:", t4)
+print("8. Membership cheak:", 30 in marks)
+lst = list(marks)
+print("9. Tuple to list:", lst)
+
